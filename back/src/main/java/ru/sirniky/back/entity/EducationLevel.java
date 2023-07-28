6eb6a285af84083cf893c1237ae47d9747f96588
alexp <alexdmitralex@gmail.com>
2023-07-28 16:54:46 +0300

    add EducationLevel Entity

---
+++ back/src/main/java/ru/sirniky/back/entity/EducationLevel.java
@@ -0,0 +1,29 @@
+package ru.sirniky.back.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Table(name = "education_level")
+public class EducationLevel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "label")
+    private String label;
+
+    @Column(name = "ugsn_code")
+    private String ugsnCode;
+
+    @Column(name = "ugsn_label")
+    private String ugsnLabel;
+
+    @Column(name = "direction_сode")
+    private String directionCode;
+}
