fbba768f60b2f8d45b16c0ddc8382a650d295bc2
alexp <alexdmitralex@gmail.com>
2023-07-28 16:54:15 +0300

    add EducationalProgram Entity

---
+++ back/src/main/java/ru/sirniky/back/entity/EducationalProgram.java
@@ -0,0 +1,33 @@
+package ru.sirniky.back.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Table(name = "educational_program")
+public class EducationalProgram {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "educational_program_id",
+            referencedColumnName = "id")
+    private EducationLevel educationLevel;
+
+
+}
