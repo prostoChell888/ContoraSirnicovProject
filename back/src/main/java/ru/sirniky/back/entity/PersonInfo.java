587e83e6f2185ee75ad750dbe5a115524e037bd5
JustAlex322 <sklyarenkoalex322@gmail.com>
2023-07-28 11:00:57 +0300

    create super class PersonInfo

---
+++ back/src/main/java/ru/sirniky/back/entity/PersonInfo.java
@@ -0,0 +1,28 @@
+package ru.sirniky.back.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@SuperBuilder
+public class PersonInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "patronymic")
+    private String patronymic;
+}
