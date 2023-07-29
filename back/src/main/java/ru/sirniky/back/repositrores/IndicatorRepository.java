d41cb8421caef7729d936b0b2931a92e09548e18
alexp <alexdmitralex@gmail.com>
2023-07-29 15:35:05 +0300

    del .gitkeep

--- back/src/main/java/ru/sirniky/back/repositrores/IndicatorRepository.java
+++ back/src/main/java/ru/sirniky/back/repositrores/IndicatorRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.sirniky.back.entity.Indicator;
 
 public interface IndicatorRepository extends JpaRepository<Indicator, Long> {
-    // You can define custom query methods here if needed
 }
