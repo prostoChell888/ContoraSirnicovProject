5ac1d32e8a90bcdbef55198af6f165858d9189c2
alexp <alexdmitralex@gmail.com>
2023-07-29 17:33:30 +0300

    add Controllers

---
+++ back/src/main/java/ru/sirniky/back/controlers/CompetenciesController.java
@@ -0,0 +1,47 @@
+package ru.sirniky.back.controlers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.sirniky.back.dto.request.CreateCompetenciesRequest;
+import ru.sirniky.back.dto.response.CompetenciesResponse;
+import ru.sirniky.back.service.CompetenciesService;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/competencies")
+@RequiredArgsConstructor
+public class CompetenciesController {
+    private final CompetenciesService competenciesService;
+
+
+
+    @GetMapping
+    public List<CompetenciesResponse> getAllCompetencies() {
+        return competenciesService.getAllCompetencies();
+    }
+
+    @GetMapping("/{id}")
+    public CompetenciesResponse getCompetenciesById(@PathVariable Long id) {
+        return competenciesService.getCompetenciesById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CompetenciesResponse createCompetencies(@RequestBody @Valid CreateCompetenciesRequest request) {
+        return competenciesService.saveCompetencies(request);
+    }
+
+    @PutMapping("/{id}")
+    public CompetenciesResponse updateCompetencies(@PathVariable Long id, @RequestBody @Valid CreateCompetenciesRequest request) {
+        return competenciesService.updateCompetencies(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCompetencies(@PathVariable Long id) {
+        competenciesService.deleteCompetencies(id);
+    }
+}
---
+++ back/src/main/java/ru/sirniky/back/controlers/EducationLevelController.java
@@ -0,0 +1,49 @@
+package ru.sirniky.back.controlers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.sirniky.back.dto.request.CreateEducationLevelRequest;
+import ru.sirniky.back.dto.response.EducationLevelResponse;
+import ru.sirniky.back.service.EducationLevelService;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/education-levels")
+@RequiredArgsConstructor
+
+public class EducationLevelController {
+    private final EducationLevelService educationLevelService;
+
+
+
+    @GetMapping
+    public List<EducationLevelResponse> getAllEducationLevels() {
+        return educationLevelService.getAllEducationLevels();
+    }
+
+    @GetMapping("/{id}")
+    public EducationLevelResponse getEducationLevelById(@PathVariable Long id) {
+        return educationLevelService.getEducationLevelById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public EducationLevelResponse createEducationLevel(@RequestBody @Valid CreateEducationLevelRequest request) {
+        return educationLevelService.saveEducationLevel(request);
+    }
+
+    @PutMapping("/{id}")
+    public EducationLevelResponse updateEducationLevel(@PathVariable Long id, @RequestBody @Valid CreateEducationLevelRequest request) {
+        return educationLevelService.updateEducationLevel(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEducationLevel(@PathVariable Long id) {
+        educationLevelService.deleteEducationLevel(id);
+    }
+}
+
