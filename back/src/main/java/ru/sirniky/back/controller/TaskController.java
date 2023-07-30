7745674766365d9f48940a82aeb43aed0b0f539a
alexp <alexdmitralex@gmail.com>
2023-07-30 11:37:44 +0300

    изменил возвращаемый статус при создании теста на 201

--- back/src/main/java/ru/sirniky/back/controller/TaskController.java
+++ back/src/main/java/ru/sirniky/back/controller/TaskController.java
@@ -1,13 +1,8 @@
 package ru.sirniky.back.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import ru.sirniky.back.entity.task.Task;
 import ru.sirniky.back.dto.TaskDto;
 import ru.sirniky.back.service.TaskService;
@@ -22,6 +17,7 @@ public class TaskController {
     private final TaskService taskServiceImpl;
 
     @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
     public void create(@RequestBody TaskDto task){
         taskServiceImpl.createTask(task);
     }
